aff79f954ed9dc10135ac83de4eb18562387c0e1
Gábor Tóth <tothgabor@Gabors-MacBook-Pro.local>
2026-05-28 23:38:01 +0200

    measure mmap

--- bench_test.go
+++ bench_test.go
@@ -24,39 +24,54 @@ func TestBufioSequential(t *testing.T) {
 	if *profile {
 		os.MkdirAll("profiles", 0755)
 	}
-	Measure("bufio_4KB", false, func() {
+	Measure("bufio_4KB", *profile, func() {
 		readBufioSize(resolveInput(*input), 4*1024)
 	})
-	Measure("bufio_500KB", false, func() {
+	Measure("bufio_500KB", *profile, func() {
 		readBufioSize(resolveInput(*input), 512*1024)
 	})
-	Measure("bufio_1MB", false, func() {
+	Measure("bufio_1MB", *profile, func() {
 		readBufioSize(resolveInput(*input), 1024*1024)
 	})
-	Measure("bufio_2MB", false, func() {
+	Measure("bufio_2MB", *profile, func() {
 		readBufioSize(resolveInput(*input), 2*1024*1024)
 	})
-	Measure("bufio_16MB", false, func() {
+	Measure("bufio_16MB", *profile, func() {
 		readBufioSize(resolveInput(*input), 16*1024*1024)
 	})
-	Measure("bufio_scanner", false, func() {
+	Measure("bufio_scanner", *profile, func() {
 		readBufioScanner(resolveInput(*input))
 	})
 }
 func TestReadSequeltial(t *testing.T) {
-	Measure("read_4KB", false, func() {
+	Measure("read_4KB", *profile, func() {
 		readChunked(resolveInput(*input), 4*1024)
 	})
-	Measure("read_500KB", false, func() {
+	Measure("read_500KB", *profile, func() {
 		readChunked(resolveInput(*input), 512*1024)
 	})
-	Measure("read_1MB", false, func() {
+	Measure("read_1MB", *profile, func() {
 		readChunked(resolveInput(*input), 1024*1024)
 	})
-	Measure("read_2MB", false, func() {
+	Measure("read_2MB", *profile, func() {
 		readChunked(resolveInput(*input), 2*1024*1024)
 	})
-	Measure("read_16MB", false, func() {
+	Measure("read_16MB", *profile, func() {
 		readChunked(resolveInput(*input), 16*1024*1024)
+	})
+}
+
+func TestMMappedRead(t *testing.T) {
+	Measure("mapped_4KB", *profile, func() {
+		readMMapped(resolveInput(*input), 4*1024)
+	})
+	Measure("mapped_1MB", *profile, func() {
+		readMMapped(resolveInput(*input), 1024*1024)
+	})
+	Measure("mapped_4MB", *profile, func() {
+		readMMapped(resolveInput(*input), 4*1024*1024)
+	})
+	Measure("mapped_16MB", *profile, func() {
+		readMMapped(resolveInput(*input), 16*1024*1024)
 	})
 }
