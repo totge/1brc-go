8dc0f8088de55f561117b693d749c1c5a0f28ffb
Gábor Tóth <tothgabor@Gabors-MacBook-Pro.local>
2026-07-02 15:50:01 +0200

    add performance testing

--- performance_test.go
+++ performance_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"1brc-go/iterations/base"
+	iter01 "1brc-go/iterations/iter_01"
 	"testing"
 )
 
@@ -12,6 +13,19 @@ func TestBaseVersion(t *testing.T) {
 	Measure("sequential_idiomatic", *profile, func() {
 		inputPath, outputPath := resolveFileSize(*input)
 		base.Execute(inputPath, outputPath, 8*1024*1024)
+	})
+	// Measure("sequential_idiomatic", *profile, func() {
+	// 	BaseExecute(resolveInput(*input), 12*1024*1024)
+	// })
+}
+
+func TestIter01(t *testing.T) {
+	// Measure("sequential_idiomatic", *profile, func() {
+	// 	BaseExecute(resolveInput(*input), 4*1024*1024)
+	// })
+	Measure("iter_01", *profile, func() {
+		inputPath, outputPath := resolveFileSize(*input)
+		iter01.Execute(inputPath, outputPath, 8*1024*1024)
 	})
 	// Measure("sequential_idiomatic", *profile, func() {
 	// 	BaseExecute(resolveInput(*input), 12*1024*1024)
